e90678fb53c979a9fa6133cd2a0cf6d8339f5513
SAKSHI PANDEY <khushipandey9549@gmail.com>
2026-07-27 22:11:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Cart from "./pages/Cart";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ProductDetails from "./pages/ProductDetails";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
