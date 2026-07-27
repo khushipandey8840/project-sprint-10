08f4301a96ec80dea4521287237094dcc50bc128
SAKSHI PANDEY <khushipandey9549@gmail.com>
2026-07-27 22:00:20 +0530

    Add files via upload

---
+++ src/store/filterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  category: "all",
+  maxPrice: 1000,
+};
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    setCategory: (state, action) => {
+      state.category = action.payload;
+    },
+    setMaxPrice: (state, action) => {
+      state.maxPrice = action.payload;
+    },
+  },
+});
+
+export const { setCategory, setMaxPrice } = filterSlice.actions;
+export default filterSlice.reducer;
